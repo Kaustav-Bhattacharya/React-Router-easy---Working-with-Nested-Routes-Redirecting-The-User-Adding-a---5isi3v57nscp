be9761d15c03307951e9e38a076aaa02b1bb3a72
Kaustav-Bhattacharya <bhattacharyar64@gmail.com>
2022-09-24 06:30:02 +0530

    updated

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../src/Pages/Home";
+import Index from "../src/Pages/Index";
+import NotFound from "../src/Pages/NotFound";
 
-export const AppRoutes = () => {
-    return (
+ const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
-    )
-}
+export default AppRoutes;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
-export const NavBar = () =>{
+ const NavBar = () =>{
     return(
-
+       <nav>
+        <ul>
+            <li>
+                <Link className='index-link' to="/">Index</Link>
+            </li>
+            <li>
+                <Link className='home-link' to='/home'>Home</Link>
+            </li>
+        </ul>
+       </nav>
     )
 }
+export default NavBar;
